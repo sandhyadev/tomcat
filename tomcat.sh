e837da7d5d401490f5f1c86b18ff947e0f30e9fb
sandhyadev <sandhyadevops26@gmail.com>
2019-11-19 15:44:49 +0530

    tomcat3

--- tomcat.sh
+++ tomcat.sh
@@ -3,5 +3,5 @@
 sudo apt update
 wget http://apache.mirrors.pair.com/tomcat/tomcat-8/v8.5.47/bin/apache-tomcat-8.5.47.tar.gz
 tar xvf apache-tomcat-8.5.47.tar.gz
-cd /home/ubuntu/apache-tomcat-8.5.47/
+cd apache-tomcat-8.5.47/
 cd bin/ && sh startup.sh
